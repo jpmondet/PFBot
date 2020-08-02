0c5f5a558360a687c7cc2aedcf8afdd0c48d15e3
jpmondet <jp@mondet.org>
2020-08-02 09:03:04 +0200

    Add command to register

--- dcbot.py
+++ dcbot.py
@@ -4,10 +4,11 @@
 
 
 import os
+import json
 import subprocess
+import requests
 import discord
 from discord.ext import commands
-#from top_accu import top_acc
 
 from dotenv import load_dotenv
 
@@ -15,6 +16,9 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv('DISCORD_GUILD')
+
+ssurl = "https://new.scoresaber.com/api/player/{}/full"
+
 
 bot = commands.Bot(command_prefix='!')
 
@@ -32,38 +36,77 @@ def paginate(lines, chars=2000):
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord and ready to get cmds')
 
 @bot.command(name='topAcc', help='Shows the current top50 of Average Ranked Accuracy')
 #@commands.has_role('admin')
-async def topAcc(ctx, nb_to_show: int = 50):
+async def topAcc(ctx): #, nb_to_show: int = 50):
     #guild = ctx.guild
     #existing_channel = discord.utils.get(guild.channels, name=channel_name)
     #if not existing_channel:
     #    print(f'Creating a new channel: {channel_name}')
     #    await guild.create_text_channel(channel_name)
-    #response = "1st : You"
-    #accu_rank = top_acc()
-    #response = "#"*15 + "Top by Accuracy" + "#"*15 + '\n'
-    #for rank, stats in enumerate(accu_rank):
-    #    if rank < 50:
-    #        print(" {:>2} - {:>30}     (ranked_accu: {:.2f}  ranked_count: {})".format(rank+1, stats[0], stats[1][1], stats[1][0]))
-
-    #print(ctx.__dict__)
     print(f"{ctx.author} asked for {ctx.message.content} on chan {ctx.channel} of {ctx.guild}")
     print("Starting to generate Acc leaderboard")
+    await ctx.send("Starting to generate Acc leaderboard, please wait...(thanks Umbranox for rate-limiting)")
     topacc_return = subprocess.run(["python3", "../ssapi/topAcc/top_accu.py"], capture_output=True, text=True)
     print(topacc_return.stdout)
-
-    #response = topacc_return.stdout
     for message in paginate(topacc_return.stdout):
         msg_to_send = ''.join(message)
         await ctx.send(msg_to_send)
+
+@bot.command(name='runs', help='Get all your runs')
+#@commands.has_role('admin')
+async def runs(ctx):
+    #guild = ctx.guild
+    #existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    #if not existing_channel:
+    #    print(f'Creating a new channel: {channel_name}')
+    #    await guild.create_text_channel(channel_name)
+    print(f"{ctx.author} asked for {ctx.message.content} on chan {ctx.channel} of {ctx.guild}")
+    print("Starting to get all runs")
+    await ctx.send("Starting to process all runs, please wait...")
+
+    topacc_return = subprocess.run(["python3", "../ssapi/topAcc/top_accu.py"], capture_output=True, text=True)
+    print(topacc_return.stdout)
+    for message in paginate(topacc_return.stdout):
+        msg_to_send = ''.join(message)
+        await ctx.send(msg_to_send)
+
+@bot.command(name='ss', help='Register with your SS account')
+async def ss(ctx, ssa):
+    print(f"{ctx.author} asked for {ctx.message.content} on chan {ctx.channel} of {ctx.guild}")
+    if not ssa:
+        await ctx.send("Please, indicate your SS account. For example `!ss 76561197964179685`")
+        return
+    
+    accounts = {}
+    with open('accounts', 'r') as facc:
+        accounts = json.load(facc)
+
+    if accounts.get(ctx.author):
+        await ctx.send("Nice (re)try but account already registered")
+        return
+
+    
+    print("Validating that the account exist")
+
+    req = requests.get(ssurl.format(ssa))
+
+    if req.status_code != 200:
+        await ctx.send("Invalid SS account")
+        return
+
+    accounts[ctx.author] = req.json()['playerInfo']['playerId']
+
+    with open('accounts', 'w') as facc:
+        json.dump(accounts, facc)
+
+    await ctx.send("Account correctly registered \\o/")
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
-
 
 bot.run(TOKEN)
