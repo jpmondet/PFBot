40bafe61b0525f56720bebdbcd9ba4f9c5bfee41
jpmondet <jp@mondet.org>
2020-08-08 07:28:32 +0200

    Add list-runs, specific run & unlink commands

--- dcbot.py
+++ dcbot.py
@@ -29,6 +29,21 @@ ssurl = "https://new.scoresaber.com/api/player/{}/full"
 
 bot = commands.Bot(command_prefix='!')
 
+def load_accounts():
+    id_accounts = {}
+    ss_accounts = {}
+
+    with open('id_accounts_list', 'r') as facc:
+        id_accounts = json.load(facc)
+    with open('ss_accounts_list', 'r') as facc:
+        ss_accounts = json.load(facc)
+    
+    return id_accounts, ss_accounts
+
+def check_and_get_ss_account(discord_id):
+    pass
+
+
 def paginate(lines, chars=2000):
     size = 0
     message = []
@@ -60,9 +75,45 @@ async def topAcc(ctx): #, nb_to_show: int = 50):
     print(topacc_return.stdout)
     for message in paginate(topacc_return.stdout):
         msg_to_send = ''.join(message)
-        await ctx.send(msg_to_send)
 
-@bot.command(name='runs', help='Get all your runs')
+@bot.command(name='list-runs', help='List all your runs')
+#@commands.has_role('admin')
+async def list_runs(ctx):
+    #guild = ctx.guild
+    #existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    #if not existing_channel:
+    #    print(f'Creating a new channel: {channel_name}')
+    #    await guild.create_text_channel(channel_name)
+    print(f"{ctx.author} asked for {ctx.message.content} on chan {ctx.channel} of {ctx.guild}")
+
+    author = str(ctx.author.id)
+
+    id_accounts, ss_accounts = load_accounts()
+
+    ssacc = id_accounts.get(author)
+
+    if not ssacc:
+        await ctx.send("Account not registered. Please use !ss YourSSaccountID to register. Exple : `!ss 76561197964179685` ")
+        return
+
+    runs_dir = f"../BSDlogs/{ssacc}"
+
+    try:
+        runs = os.listdir(runs_dir)
+    except FileNotFoundError:
+        await ctx.send("No runs saved for this account")
+        return
+
+    if not runs:
+        await ctx.send("No runs saved for this account")
+        return
+
+    await ctx.send("Here are your save runs : ")
+    for run in runs:
+        await ctx.send(run)
+
+
+@bot.command(name='runs', help='Process and returns details from all your runs')
 #@commands.has_role('admin')
 async def runs(ctx):
     #guild = ctx.guild
@@ -71,18 +122,15 @@ async def runs(ctx):
     #    print(f'Creating a new channel: {channel_name}')
     #    await guild.create_text_channel(channel_name)
     print(f"{ctx.author} asked for {ctx.message.content} on chan {ctx.channel} of {ctx.guild}")
-    accounts = {}
-    with open('accounts_list', 'r') as facc:
-        accounts = json.load(facc)
+    author = str(ctx.author.id)
 
-    print(accounts)
-    print(ctx.author.id)
+    id_accounts, ss_accounts = load_accounts()
 
-    if not accounts.get(str(ctx.author.id)):
+    ssacc = id_accounts.get(author)
+
+    if not ssacc:
         await ctx.send("Account not registered. Please use !ss YourSSaccountID to register. Exple : `!ss 76561197964179685` ")
         return
-
-    ssacc = accounts.get(str(ctx.author.id))
 
     runs_dir = f"../BSDlogs/{ssacc}"
 
@@ -99,9 +147,47 @@ async def runs(ctx):
     print("Starting to get all runs")
     await ctx.send("Starting to process all runs, please wait...")
 
-    topacc_return = subprocess.run(["python3", "../bsdlp/parse_logs.py", "-d", f"{runs_dir}", "-c", "True", "-nc", "True"], capture_output=True, text=True)
-    print(topacc_return.stdout)
-    for message in paginate(topacc_return.stdout):
+    output = subprocess.run(["python3", "../bsdlp/parse_logs.py", "-d", f"{runs_dir}", "-c", "True", "-nc", "True"], capture_output=True, text=True)
+    print(output.stdout)
+    for message in paginate(output.stdout):
+        msg_to_send = ''.join(message)
+        await ctx.send(msg_to_send)
+
+@bot.command(name='run', help='Process and returns details from the run you specified')
+#@commands.has_role('admin')
+async def show_run(ctx, run_to_process):
+    #guild = ctx.guild
+    #existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    #if not existing_channel:
+    #    print(f'Creating a new channel: {channel_name}')
+    #    await guild.create_text_channel(channel_name)
+    print(f"{ctx.author} asked for {ctx.message.content} on chan {ctx.channel} of {ctx.guild}")
+
+    if not run_to_process:
+        await ctx.send("Please, indicate which run you want to show. For exple : `!run 2020-08-02-19-30-45`")
+        return
+
+    author = str(ctx.author.id)
+
+    id_accounts, ss_accounts = load_accounts()
+
+    ssacc = id_accounts.get(author)
+
+    if not ssacc:
+        await ctx.send("Account not registered. Please use !ss YourSSaccountID to register. Exple : `!ss 76561197964179685` ")
+        return
+
+    run_file = f"../BSDlogs/{ssacc}/{run_to_process}"
+
+    if not os.access(run_file, os.R_OK):
+        await ctx.send("This run cannot be found. You can check which runs you have with `!runs`")
+        return
+
+    print(f"Starting to process run {run_file}")
+
+    output = subprocess.run(["python3", "../bsdlp/parse_logs.py", "-f", f"{run_file}", "-c", "True", "-nc", "True"], capture_output=True, text=True)
+    print(output.stdout)
+    for message in paginate(output.stdout):
         msg_to_send = ''.join(message)
         await ctx.send(msg_to_send)
 
@@ -111,17 +197,21 @@ async def ss(ctx, ssa):
     if not ssa:
         await ctx.send("Please, indicate your SS account. For example `!ss 76561197964179685`")
         return
-    
-    accounts = {}
-    with open('accounts_list', 'r') as facc:
-        accounts = json.load(facc)
 
-    print(accounts)
-    print(ctx.author.id)
-    if accounts.get(str(ctx.author.id)):
+    author = str(ctx.author.id)
+
+    id_accounts, ss_accounts = load_accounts()
+
+    ssacc = id_accounts.get(author)
+    iddisc = ss_accounts.get(ssa)
+
+    if ssacc:
         await ctx.send("Nice (re)try but account already registered")
         return
 
+    if iddisc:
+        await ctx.send("Hmm, this SS account is already registered to another account...o.0")
+        return
     
     print("Validating that the account exist")
 
@@ -131,13 +221,48 @@ async def ss(ctx, ssa):
         await ctx.send("Invalid SS account")
         return
 
-    accounts[str(ctx.author.id)] = req.json()['playerInfo']['playerId']
-    print(accounts)
+    player_id = req.json()['playerInfo']['playerId']
+    id_accounts[author] = player_id
+    ss_accounts[player_id] = author
+    print(id_accounts)
+    print(ss_accounts)
 
-    with open('accounts_list', 'w') as facc:
-        json.dump(accounts, facc)
+    with open('id_accounts_list', 'w') as fidacc:
+        json.dump(id_accounts, fidacc)
+
+    with open('ss_accounts_list', 'w') as fssacc:
+        json.dump(ss_accounts, fssacc)
 
     await ctx.send("Account correctly registered :+1:")
+
+@bot.command(name='unlink', help='Unlink SS account from your discord account')
+async def unlink(ctx):
+    print(f"{ctx.author} asked for {ctx.message.content} on chan {ctx.channel} of {ctx.guild}")
+
+    author = str(ctx.author.id)
+
+    id_accounts, ss_accounts = load_accounts()
+
+    ssacc = id_accounts.get(author)
+
+    if not ssacc:
+        await ctx.send("Wait, you didn't register your SS account in the first place..")
+        return
+
+    print(f"Unlinking {ssacc} from {author}")
+
+    del(id_accounts[author])
+    del(ss_accounts[ssacc])
+    print(id_accounts)
+    print(ss_accounts)
+
+    with open('id_accounts_list', 'w') as fidacc:
+        json.dump(id_accounts, fidacc)
+
+    with open('ss_accounts_list', 'w') as fssacc:
+        json.dump(ss_accounts, fssacc)
+
+    await ctx.send("Account correctly unlinked... So sad :crying_cat_face:")
 
 @bot.event
 async def on_command_error(ctx, error):
